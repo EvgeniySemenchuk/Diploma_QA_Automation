4c7cdcb6d802871b15b73f7f140bd205db7aac8e
EvgeniySemenchuk <evgeniy.semenchuk.2004@gmail.com>
2024-03-09 20:43:18 +0300

    Ninth commit(new tests and new described pages)

--- src/main/java/pageObject/wildberries/NavigationBarPage.java
+++ src/main/java/pageObject/wildberries/NavigationBarPage.java
@@ -11,7 +11,7 @@ public class NavigationBarPage extends BaseWBPage<NavigationBarPage> {
     }
 
     private String getThirdCategory(String firstCategory, String secondCategory, String thirdCategory) {
-        return getSecondCategory(firstCategory,secondCategory).concat("//ancestor::*[@data-tag=\"pagesContainer\"]//div[@data-level=\"2\"]//span[contains(.,\""+thirdCategory+"\")]")
+        return getSecondCategory(firstCategory,secondCategory).concat("//ancestor::*[@data-tag=\"pagesContainer\"]//div[@data-level=\"2\"]//span[contains(.,\""+thirdCategory+"\")]");
     }
 
     private String getFourthCategory(String firstCategory, String secondCategory, String thirdCategory, String fourthCategory) {
